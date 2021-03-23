325694f7c556b5eb7c623b877042d06589b10f14
parksangwoo1617 <b01056721617@gmail.com>
2021-03-23 23:31:19 +0900

    [CREATE] app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.set('port', process.env.PORT || 8080);
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+}));
