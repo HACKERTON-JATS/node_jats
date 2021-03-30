cd7806514b53793c37c64dd4ff00b04ce985858d
parksangwoo <b01056721617@gmail.com>
2021-03-30 17:03:03 +0900

    [CREATE] user.js

---
+++ controller/user.js
@@ -0,0 +1,22 @@
+const CampaignFile = require('../models/campaignFile');
+
+const postCampaignFile = async(req, res) => {
+    try {
+        
+    } catch(err) {
+
+    }
+};
+
+const postComment = async(req, res) => {
+    try {
+
+    } catch(err) {
+
+    }
+};
+
+module.exports = {
+    postCampaignFile,
+    postComment
+}
