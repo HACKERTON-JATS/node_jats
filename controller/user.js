7ad8b4d15d0d3cefbbd9278acb7c83e7d88c0342
parksangwoo <b01056721617@gmail.com>
2021-03-31 17:05:32 +0900

    [PATCH] user.js

--- controller/user.js
+++ controller/user.js
@@ -16,7 +16,7 @@ const postComment = async(req, res) => {
     config.upload.single('commentFile')(req, res, (err) => {
         CommentFile.create({
             path: req.file.filename,
-            
+            comment_id: req.params.campaignId
         }).then(result => res.json(result))
         .catch(err => res.json(err));
     })
